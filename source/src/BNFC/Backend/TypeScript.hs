412316166e37b924ba62e3c8700bfec57cd0e114
Michael Dmitriev <m.dmitriev@innopolis.university>
2023-10-08 17:30:33 +0300

    [TypeScript] added generation of package.json file

--- source/src/BNFC/Backend/TypeScript.hs
+++ source/src/BNFC/Backend/TypeScript.hs
@@ -1,24 +1,58 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module BNFC.Backend.TypeScript ( makeTypeScript ) where
 
-import Control.Monad.Trans.Writer (tell, WriterT)
+import Prelude hiding ((<>))
+import Text.PrettyPrint ( text, vcat, Doc )
+import System.FilePath ((</>), pathSeparator)
+import Data.Char (toLower)
 
 import BNFC.Backend.Base
 import BNFC.CF
-import BNFC.Options (SharedOptions)
+import BNFC.Options (SharedOptions (Options, inPackage, lang))
+import BNFC.Utils (mkName, NameStyle (CamelCase), replace, (+.+))
 
 makeTypeScript :: SharedOptions -> CF -> MkFiles ()
-makeTypeScript _ _ = writeTsIndexFile
-
-writeTsIndexFile :: WriterT [GeneratedFile] IO()
-writeTsIndexFile = do
-    tell [tsIndexFile]
-    return ()
+makeTypeScript Options{..} _ = do
+    let packageBase = maybe id (+.+) inPackage pkgName
+        dirBase = pkgToDir packageBase
+ 
+    mkfile (dirBase </> "package.json") makeJsonComment packageJsonContent
   where
-    tsIndexFile = GeneratedFile
-      { fileName    = "index.ts"
-      , fileContent = "console.log('Hello world')"
-      , makeComment = makeCommentLine
-      }
+    pkgName = mkName [] CamelCase lang
+    pkgToDir = replace '.' pathSeparator
 
-makeCommentLine :: String -> String
-makeCommentLine = ("// " ++)
+    packageJsonContent = vcat
+      [ "{"
+      , indent 2 $ "\"name\": \"" ++ toLowerCase lang ++ "\","
+      , indent 2 "\"version\": \"1.0.0\","
+      , indent 2 "\"description\": \"\","
+      , indent 2 "\"main\": \"index.ts\","
+      , indent 2 "\"scripts\": {"
+      , indent 4 "\"run\": \"ts-node index.ts\""
+      , indent 2 "},"
+      , indent 2 "\"keywords\": [],"
+      , indent 2 "\"author\": \"\","
+      , indent 2 "\"license\": \"ISC\","
+      , indent 2 "\"dependencies\": {"
+      , indent 4 "\"antlr4\": \"^0.5.0-alpha.4\""
+      , indent 2 "},"
+      , indent 2 "\"devDependencies\": {"
+      , indent 4 "\"ts-node\": \"^10.9.1\","
+      , indent 4 "\"typescript\": \"^5.2.2\""
+      , indent 2 "}"
+      , "}"
+      ]
+
+makeTsComment :: String -> String
+makeTsComment = ("// -- TypeScript -- " ++)
+
+makeJsonComment :: String -> String
+makeJsonComment = ("// -- JSON -- " ++)
+
+-- add N spaces before line
+indent :: Int -> String -> Doc
+indent size = text . (replicate size ' ' ++)
+
+toLowerCase :: String -> String
+toLowerCase = map toLower
